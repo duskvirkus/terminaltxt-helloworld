ffff29a72f0ce32d28fd1a390e98af8f6dab3986
figraham <fi.sage.graham@gmail.com>
2019-04-16 20:08:21 -0600

    updating hello world to 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-import * as terminaltxt from '../../../build/dev/terminaltxt';
+import { GraphicsTerminal, CharacterSet, TerminalConfig } from 'terminaltxt';
 
-terminaltxt.sayHello('World!');
-terminaltxt.addHello(2, 2);
+if (document.readyState === 'complete') {
+  init();
+} else {
+  window.addEventListener('load', init, false);
+}
+
+let term: GraphicsTerminal;
+
+function init(): void {
+  term = new GraphicsTerminal(
+    {
+      container: document.getElementById('container')
+    } as TerminalConfig,
+    new CharacterSet('Hello World!')
+  );
+  update();
+}
+
+function update(): void {
+  setTimeout(() => {
+    term.update();
+    for (let i: number = 0; i < 10; i++) {
+      term.setCell(
+        Math.floor(Math.random() * term.getCharacterSet().size()),
+        Math.floor(Math.random() * term.getWidth()),
+        Math.floor(Math.random() * term.getHeight())
+      );
+    }
+    update();
+  }, 30);
+}
